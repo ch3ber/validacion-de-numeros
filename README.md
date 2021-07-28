b388f1accae013a41d5b7eb872acfd1e8ed182e5
Eber Alejo <eber4202@hotmail.com>
2021-07-27 20:27:33 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+#Pequeño problema de javascript resuelto
+
+El problema consiste en:
+
+Ingrese una cifra mínima de 6 dígitos enteros (debe ser ingresado como
+número no como cadena de caracteres, se debe realizar la verificación de
+que solo se ingresen números), la cual no debe tener dígitos repetidos
+(realizar el método de comprobación de ambos casos), una vez comprobado
+que no existe dígitos repetidos, proceda a separar digito a digito y
+guardarlos en un array
+
+![user interface](src/img/UI.png)
---
+++ app.js
@@ -0,0 +1,49 @@
+'use strict';
+
+function verificacion() {
+   const display = document.querySelector('.input-numbers');
+   const contenidoDisplay = display.value;
+
+   // mover el contenido del display a un array
+   const numeros = [];
+   for (let i = 0; i <= contenidoDisplay.length - 1; i++) {
+      numeros.push(display.value[i]); 
+   }
+
+   if (! Number.isInteger(parseFloat(contenidoDisplay))) {
+      mostrarMensaje('El numero no es entero');
+      return;
+   }
+   if (contenidoDisplay.length < 6) {
+      mostrarMensaje('El numero es menor a 6 digitos o no es un numero');
+      return;
+   }
+   if (numeros.some(numerosRepetidos)) {
+      mostrarMensaje('El numero tinene numeros repetidos');
+      return;
+   }
+   mostrarMensaje('El numero cumple con todas las reglas', 'valido');
+}
+
+function numerosRepetidos(valor, indice, lista) {
+   return !(lista.indexOf(valor) == indice);
+}
+
+function mostrarMensaje(mensaje, tipo = null) {
+   const contenidoApp = document.querySelector('.contenido');
+   const contendorMensaje = document.createElement('P');
+   
+   contendorMensaje.textContent = mensaje;
+   if (!tipo) {
+      contendorMensaje.classList.add('error');
+   }
+   if (tipo) {
+      contendorMensaje.classList.add('valido');
+   }
+   contenidoApp.appendChild(contendorMensaje);
+
+   setTimeout(() => {
+      contendorMensaje.remove();
+   }, 1000)
+}
+
